3b4fb517de119bd948966e961f73cb15b390e161
Abián Izquierdo <abizmo@gmail.com>
2023-02-26 21:33:21 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      Your Result 76 of 100 Great You scored higher than 65% of the people who
+      have taken these tests. Summary Reaction 80 / 100 Memory 92 / 100 Verbal
+      61 / 100 Visual 72 / 100 Continue
+      <footer className="attribution">
+        Challenge by{' '}
+        <a
+          href="https://www.frontendmentor.io?ref=challenge"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Frontend Mentor
+        </a>
+        . Coded by <a href="https://www.abizmo.dev">Abián Izquierdo</a>.
+      </footer>
+    </div>
+  );
+}
+
+export default App;
