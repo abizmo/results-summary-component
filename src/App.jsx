3ea31a9edfb6a354223b5071e5b4dc6c901081b1
Abián Izquierdo <abizmo@gmail.com>
2023-02-27 18:09:07 +0000

    feat(footer): Add style with tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -6,16 +6,21 @@ function App() {
       Your Result 76 of 100 Great You scored higher than 65% of the people who
       have taken these tests. Summary Reaction 80 / 100 Memory 92 / 100 Verbal
       61 / 100 Visual 72 / 100 Continue
-      <footer className="attribution">
+      <footer className="text-xs text-center">
         Challenge by{' '}
         <a
           href="https://www.frontendmentor.io?ref=challenge"
           target="_blank"
           rel="noreferrer"
+          className="text-primary-blue"
         >
           Frontend Mentor
         </a>
-        . Coded by <a href="https://www.abizmo.dev">Abián Izquierdo</a>.
+        . Coded by{' '}
+        <a className="text-primary-blue" href="https://www.abizmo.dev">
+          Abián Izquierdo
+        </a>
+        .
       </footer>
     </div>
   );
