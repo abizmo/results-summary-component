40643858c0f3ad1f3e9784cbf233567aae3d5fd9
abizmo <abizmo@gmail.com>
2023-02-27 13:02:53 +0000

    feat: add theme font config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -16,7 +16,22 @@ module.exports = {
       'violet-blue': 'hsla(256, 72%, 46%, 1)',
       'persian-blue': 'hsla(241, 72%, 46%, 0)',
     },
+    fontSize: {
+      xs: ['.6875rem', '1.3'],
+      sm: ['1rem', '1'],
+      base: ['1.125rem', '1'],
+      lg: ['1.5rem', '1'],
+      xl: ['2rem', '1'],
+      '2xl': ['3.5rem', '1'],
+      '3xl': ['4.5rem', '1'],
+    },
     extend: {
+      letterSpacing: {
+        wide: '0.01em',
+      },
+      lineHeight: {
+        tight: '1.3',
+      },
       fontFamily: {
         sans: ['Hanken Grotesk', 'sans-serif'],
       },
